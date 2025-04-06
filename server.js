d82f923430067bb493f759a450a2539e61df0a96
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2025-04-06 17:21:54 +0530

    templating engine

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ const about = require('./routes/about.routes')
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use((req, res, next) => {
     let start = Date.now();
     next();
     let time = Date.now() - start;
     console.log(`${req.method} ${req.baseUrl} ${req.url} ${time}ms`);
-}); 
+});
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'New Home page',
+        heading: 'Hello, this is the home page.'
+    })
+})
 app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use("/users", users);
